333a81756efb39caf96ab7fe4a01bd79afc6d161
Tassia Barros <tassinha.cpr@gmail.com>
2020-05-17 23:14:21 -0300

    Completeness_technique

---
+++ Matrix_manipulation/completeness_technique.py
@@ -0,0 +1,28 @@
+import numpy as np
+# noinspection PyUnresolvedReferences
+import pandas as pd
+
+
+class Techniques:
+    @staticmethod
+    def technique_1(cells, data, tech):
+        tech_complete = 0
+        if (tech == "average"):
+            average = data.mean()
+            for i in range(0, len(cells)):
+                data[cells[i][0]][cells[i][1]] = average
+
+        elif (tech == "median"):
+            median = data.median()
+            for i in range(0, len(cells)):
+                data[cells[i][0]][cells[i][1]] = median
+        else:
+            for i in range(0, len(cells)):
+                data[cells[i][0]][cells[i][1]] = 0
+                print()
+        return data
+
+
+
+
+
